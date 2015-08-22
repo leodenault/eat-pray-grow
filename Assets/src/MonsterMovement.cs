1673f84feaa2b4e54647324ba19fdfab54b9b721
Vanessa MacIsaac <vsmacisaac@gmail.com>
2015-08-22 15:34:52 -0400

    smoothing rotation

--- Assets/src/MonsterMovement.cs
+++ Assets/src/MonsterMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MonsterMovement : MonoBehaviour {
 
 	private const float speed = 1.8f;
+	public float rotationDegreesPerSecond = 10f;
 
 	public GameObject monster;
 	public Camera cam;
@@ -18,20 +19,60 @@ public class MonsterMovement : MonoBehaviour {
 		// arrow key rotation control
 		float distance = Time.deltaTime * speed;
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			monster.transform.rotation = Quaternion.Euler(0,0,90);
+			float currentAngle = monster.transform.rotation.eulerAngles.z;
+			if(Mathf.Abs(90-currentAngle) <= 5){
+				monster.transform.rotation = Quaternion.Euler(0,0,90);
+			} else if(currentAngle > 90 && currentAngle < 270){
+				RotateCWise();
+			} else {
+				RotateCountCWise();
+			}
 			cam.transform.Translate(new Vector3(-distance, 0, 0));
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			monster.transform.rotation = Quaternion.Euler(0,0,180);
+			float currentAngle = monster.transform.rotation.eulerAngles.z;
+			if(Mathf.Abs(180-currentAngle) <= 5){
+				monster.transform.rotation = Quaternion.Euler(0,0,180);
+			} else if(currentAngle >= 0 && currentAngle < 180){
+				RotateCountCWise();
+			} else {
+				RotateCWise();
+			}
 			cam.transform.Translate(new Vector3(0,-distance,0));
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			monster.transform.rotation = Quaternion.Euler(0,0,270);
+			float currentAngle = monster.transform.rotation.eulerAngles.z;
+			if(Mathf.Abs(270-currentAngle) <= 5){
+				monster.transform.rotation = Quaternion.Euler(0,0,270);
+			} else if(currentAngle < 270 && currentAngle >= 90){
+				RotateCountCWise();
+			} else {
+				RotateCWise();
+			}
 			cam.transform.Translate(new Vector3(distance,0,0));
 		} else if (Input.GetKey (KeyCode.UpArrow)) {
-			monster.transform.rotation = Quaternion.Euler(0,0,0);
+			float currentAngle = monster.transform.rotation.eulerAngles.z;
+			if(Mathf.Abs(0-currentAngle) <= 5){
+				monster.transform.rotation = Quaternion.Euler(0,0,0);
+			} else if(currentAngle > 0 && currentAngle <= 180){
+				RotateCWise();
+			} else {
+				RotateCountCWise();
+			}
 			cam.transform.Translate(new Vector3(0,distance,0));
 		}
 
 
+	}
+
+	void RotateCountCWise(){
+		float currentAngle = monster.transform.rotation.eulerAngles.z;
+		float newAngle = currentAngle + rotationDegreesPerSecond;
+		monster.transform.rotation = Quaternion.Euler (0, 0, newAngle);
+	}
+
+	void RotateCWise(){
+		float currentAngle = monster.transform.rotation.eulerAngles.z;
+		float newAngle = currentAngle - rotationDegreesPerSecond;
+		monster.transform.rotation = Quaternion.Euler (0, 0, newAngle);
 	}
 
 }
