db4231814973a10bebb8466356478d46b316f978
Mathieu Jobin <mathieujobin@Mathieus-MacBook-Pro.local>
2015-08-22 14:29:55 -0400

    More levels

--- Assets/src/LevelManager.cs
+++ Assets/src/LevelManager.cs
@@ -2,20 +2,35 @@
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
+	
+	public FoodMonster monster;
 
-	public GameObject[] levels;
-
-	private GameObject currentLevel;
+	public Level currentLevel;
 
 	// Use this for initialization
 	void Start () {
-		this.currentLevel = levels[0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		double currentFood = monster.getCurrentFood();
+		double requiredFood = currentLevel.getRequiredTransitionFood();
+
+		if (currentFood > requiredFood) {
+			this.transitionToNextLevel();
+		}
 
 	
+	}
+
+	void transitionToNextLevel(){
+
+
+		this.currentLevel.Hide ();
+		this.currentLevel = currentLevel.getNextLevel();
+		this.currentLevel.Show ();
+
+
 	}
 }
