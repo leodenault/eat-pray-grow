429c3e17eeccf126a8108b13d67fb2b8e18068a7
Vanessa MacIsaac <vsmacisaac@gmail.com>
2015-08-22 13:34:00 -0400

    adding speed to transitions

--- Assets/src/MonsterMovement.cs
+++ Assets/src/MonsterMovement.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class MonsterMovement : MonoBehaviour {
 
+	private const float speed = 0.05f;
+
 	public GameObject monster;
 	public Camera cam;
 
@@ -14,18 +16,18 @@ public class MonsterMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// arrow key rotation control
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,90);
-			cam.transform.Translate(new Vector3(-1, 0, 0));
-		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			cam.transform.Translate(new Vector3(-speed, 0, 0));
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,180);
-			cam.transform.Translate(new Vector3(0,-1,0));
-		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			cam.transform.Translate(new Vector3(0,-speed,0));
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,270);
-			cam.transform.Translate(new Vector3(1,0,0));
-		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			cam.transform.Translate(new Vector3(speed,0,0));
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,0);
-			cam.transform.Translate(new Vector3(0,1,0));
+			cam.transform.Translate(new Vector3(0,speed,0));
 		}
 
 
