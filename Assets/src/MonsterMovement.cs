ae58c0bc2e1662599f590f93c98a40d6f9380558
Vanessa MacIsaac <vsmacisaac@gmail.com>
2015-08-22 13:09:20 -0400

    monster moves around now

--- Assets/src/MonsterMovement.cs
+++ Assets/src/MonsterMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MonsterMovement : MonoBehaviour {
 
 	public GameObject monster;
+	public Camera cam;
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +16,18 @@ public class MonsterMovement : MonoBehaviour {
 		// arrow key rotation control
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,90);
+			cam.transform.Translate(new Vector3(-1, 0, 0));
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,180);
+			cam.transform.Translate(new Vector3(0,-1,0));
 		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,270);
+			cam.transform.Translate(new Vector3(1,0,0));
 		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			monster.transform.rotation = Quaternion.Euler(0,0,0);
+			cam.transform.Translate(new Vector3(0,1,0));
 		}
+
 
 	}
 }
