df62e94e6c6abc0dc7c53bee2fef67701c56479b
Vanessa MacIsaac <vsmacisaac@gmail.com>
2015-08-22 12:36:47 -0400

    rotation controlled by arrow keys

---
+++ Assets/src/MonsterMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterMovement : MonoBehaviour {
+
+	public GameObject monster;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// arrow key rotation control
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			monster.transform.rotation = Quaternion.Euler(0,0,90);
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			monster.transform.rotation = Quaternion.Euler(0,0,180);
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			monster.transform.rotation = Quaternion.Euler(0,0,270);
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			monster.transform.rotation = Quaternion.Euler(0,0,0);
+		}
+
+	}
+}
