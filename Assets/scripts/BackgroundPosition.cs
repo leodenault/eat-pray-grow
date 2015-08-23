043f9d896a0e9e6fcb734c6997136eee069d3929
Katherine ChengLi <katchengli@gmail.com>
2015-08-23 13:04:02 -0400

    Background Positioning files

---
+++ Assets/scripts/BackgroundPosition.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BackgroundPosition : MonoBehaviour {
+
+	public Transform monster;
+
+	public List<RectTransform> planes = new List<RectTransform>();
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i=0; i<planes.Count; i++) {
+			if(delta (monster.position.x, planes[i].position.x) < -planes[i].rect.width){
+				planes[i].Translate(Vector3.right * -2 * planes[i].rect.width);
+			}else if(delta (monster.position.x, planes[i].position.x) > planes[i].rect.width){
+				planes[i].Translate(Vector3.right * 2 * planes[i].rect.width);
+			}
+
+			if(delta (monster.position.y, planes[i].position.y) < -planes[i].rect.height){
+				// Translation is made in z because of the rotation of the plane
+				planes[i].Translate(Vector3.forward * -2 * planes[i].rect.height);
+			}else if(delta (monster.position.y, planes[i].position.y) > planes[i].rect.height){
+				// Translation is made in z because of the rotation of the plane
+				planes[i].Translate(Vector3.forward * 2 * planes[i].rect.height);
+			}
+		}
+	}
+
+	float delta(float first, float second){
+		float sum = first - second;
+
+		return sum;
+	}
+}
