93ffc404f3c516ae7ebee8c7d1bd8845f3e7b45c
Leo Denault <leo.denault@gmail.com>
2015-08-25 23:24:35 -0400

    I BRAKED DEEZ KUFFS!!!!!!

--- Assets/src/Music.cs
+++ Assets/src/Music.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class Music : MonoBehaviour {
 
 	private const float LOOP_START = 63.75f;
+	private const float UNITY_SUCKS_AT_LIFE_GAP = 0.036f;
+	private const float LOOP_END = 108.75f - UNITY_SUCKS_AT_LIFE_GAP;
 
 	private static AudioSource INSTANCE;
-
+	private static AudioSource SWAP;
 	private static bool paused = false;
+	private static bool isTransitioning = false;
 
 	public AudioSource music;
+	public AudioSource swap;
 
 	// Use this for initialization
 	void Start () {
 		if (INSTANCE == null) {
 			INSTANCE = music;
+			SWAP = swap;
 			DontDestroyOnLoad(INSTANCE);
+			DontDestroyOnLoad(SWAP);
 			INSTANCE.Play();
 		}
 	}
@@ -24,8 +30,16 @@ public class Music : MonoBehaviour {
 		paused = !paused;
 		if (paused) {
 			INSTANCE.Pause();
+
+			if (isTransitioning) {
+				SWAP.Pause();
+			}
 		} else {
 			INSTANCE.Play();
+
+			if (isTransitioning) {
+				SWAP.Play();
+			}
 		}
 	}
 
@@ -35,8 +49,19 @@ public class Music : MonoBehaviour {
 	}
 
 	void Update() {
-		if (!INSTANCE.isPlaying && !paused) {
-			playLooped();
+		if (!paused) {
+			if (INSTANCE.time > LOOP_END && !SWAP.isPlaying) {
+				isTransitioning = true;
+				SWAP.time = LOOP_START;
+				SWAP.Play();
+			}
+
+			if (!INSTANCE.isPlaying) {
+				isTransitioning = false;
+				AudioSource temp = INSTANCE;
+				INSTANCE = SWAP;
+				SWAP = temp;
+			}
 		}
 	}
 }
